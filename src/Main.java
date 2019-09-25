184235d2fe8af8b12bf60fc649a6edcd7bd72a24
student <samoilovid951@gmail.com>
2019-09-25 11:20:57 +0300

    closes #2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int fullTank = 100;
-        int way = 1000;
-        double distance = 100.00;
-        double consumption = fullTank*distance/way;
-        System.out.println(consumption);
+        double consumption = 5.1;
+        double goTheWay = fullTank*consumption;
+        System.out.println(goTheWay);
 
-        double passedWay = 500.00;
-        double tank = fullTank - consumption*passedWay/distance;
-        System.out.println(tank);
-        double estimatedBalanceWay = tank*consumption;
-        System.out.println(estimatedBalanceWay);
 
     }
 }
