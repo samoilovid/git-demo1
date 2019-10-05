ebe848975ef7ab01cf990ae6e991da4836278f4c
student <samoilovid951@gmail.com>
2019-10-05 21:44:31 +0300

    fixes #3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int fullTank = 100;
+        int fullTank = 55;
         double hundred=100.00;
         double consumption = 5.1;
         double goTheWay = fullTank*consumption*hundred;
+        int way = (int) goTheWay;
         System.out.println(consumption);
-        System.out.println(goTheWay);
-
+        System.out.println(way);
 
     }
 }
